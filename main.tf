067bdb3f5fda1dc0ab2ffb81884cd07f5f85e4e5
Pablo Ponce <pablo@momoconsulting.net>
2022-05-22 11:15:04 +0200

    Replacing content

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ resource "aws_s3_bucket" "b" {
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.b.bucket
   key    = "index.html"
-  content = "Hola Mundo"
+  content = "Adios Mundo"
 }
