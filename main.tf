85373daa3e62cabdba37174aa6a14d24e2804a84
Pablo Ponce <pablo@momoconsulting.net>
2022-05-22 11:12:39 +0200

    Changing bucket tags

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 resource "aws_s3_bucket" "b" {
   bucket = "tf-actions-lab"
-
-  tags = {
-    Name        = "Terraform Cloud & Github Actions"
-    Environment = "Dev"
-  }
 }
 
 resource "aws_s3_object" "object" {
